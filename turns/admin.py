e65f32e96cc180bae91ba444b26fb8bd9ddd0ab6
MalditoValero <abenjamin.v@gmail.com>
2017-08-22 20:03:41 -0300

    adding models turns, day, week

--- turns/admin.py
+++ turns/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Turn,Day,Week
 
 # Register your models here.
+
+admin.site.register(Turn)
+admin.site.register(Day)
+admin.site.register(Week)
--- turns/models.py
+++ turns/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Turn(models.Model):
+    id = models.IntegerField(primary_key=True)
+    date = models.DateTimeField()
+    available = models.BooleanField(default=True)
+    taken_by = models.TextField()
+
+class Day(models.Model):
+    total_turns = models.IntegerField()
+    disable_turns = models.IntegerField(default=False)
+    taken_turns = models.IntegerField()
+    untaken_turns = models.IntegerField()
+    available = models.BooleanField(default=True)
+
+class Week(models.Model):
+    id = models.IntegerField(primary_key=True) # Number of the week per year. Range = [0,51]
+    total_days = models.IntegerField()
+
+
+
